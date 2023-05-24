de71eddd7400ab70cc91ed32ff758cd8ceb5cb9c
Yfunck <yoh-fun@hotmail.fr>
2023-05-24 19:54:19 +0200

    Entites
    Ajout des entites
    - Horaire
    - Type Horaire

---
+++ src/Entity/Horaire.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\HoraireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HoraireRepository::class)]
+#[ApiResource]
+class Horaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateHeureDebut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateHeureFin = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?TypeHoraire $typeHoraire = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $priorite = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateCreation = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $derniereModification = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(\DateTimeInterface $dateHeureDebut): self
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDateHeureFin(): ?\DateTimeInterface
+    {
+        return $this->dateHeureFin;
+    }
+
+    public function setDateHeureFin(\DateTimeInterface $dateHeureFin): self
+    {
+        $this->dateHeureFin = $dateHeureFin;
+
+        return $this;
+    }
+
+    public function getTypeHoraire(): ?TypeHoraire
+    {
+        return $this->typeHoraire;
+    }
+
+    public function setTypeHoraire(?TypeHoraire $typeHoraire): self
+    {
+        $this->typeHoraire = $typeHoraire;
+
+        return $this;
+    }
+
+    public function getPriorite(): ?int
+    {
+        return $this->priorite;
+    }
+
+    public function setPriorite(int $priorite): self
+    {
+        $this->priorite = $priorite;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getDerniereModification(): ?\DateTimeInterface
+    {
+        return $this->derniereModification;
+    }
+
+    public function setDerniereModification(?\DateTimeInterface $derniereModification): self
+    {
+        $this->derniereModification = $derniereModification;
+
+        return $this;
+    }
+}
